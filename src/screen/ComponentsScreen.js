e70cd5c686a355fa1fcd16e891c2d5f8246db6cb
Shams Raza <shamsraza250@gmail.com>
2026-03-01 17:37:05 +0530

    created dashboard screen

---
+++ src/screen/ComponentsScreen.js
@@ -0,0 +1,26 @@
+import React from "react";
+import { Text, StyleSheet } from "react-native";
+import { View } from "react-native";
+//create the components
+const ComponentsScreens = () => {
+    const name = 'Raza'
+  return (
+    <View>
+    <Text style={styles.textStyle}>Getting started with react native !</Text>
+    <Text style={styles.subHeaderStyle}>My name is {name}</Text>
+    </View>
+  );
+};
+
+// creates the styles
+const styles = StyleSheet.create({
+  textStyle: {
+    fontSize: 45,
+  },
+  subHeaderStyle:{
+    fontSize:20
+  }
+});
+
+//exports the components
+export default ComponentsScreens
