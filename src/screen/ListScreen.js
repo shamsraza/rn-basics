e7607411a30edd84da7bc9601cf0005849b181cf
Shams Raza <shamsraza250@gmail.com>
2026-03-01 19:09:19 +0530

    added age to the list

--- src/screen/ListScreen.js
+++ src/screen/ListScreen.js
@@ -3,12 +3,12 @@ import { View, Text, StyleSheet, FlatList } from "react-native";
 
 const ListScreen = () => {
   const friends = [
-    { name: "Friends 1" },
-    { name: "Friends 2" },
-    { name: "Friends 3" },
-    { name: "Friends 4" },
-    { name: "Friends 5" },
-    { name: "Friends 6" },
+    { name: "Friends 1" ,age:"20"},
+    { name: "Friends 2" ,age:"30"},
+    { name: "Friends 3" ,age:"35"},
+    { name: "Friends 4" ,age:"33"},
+    { name: "Friends 5" ,age:"23"},
+    { name: "Friends 6" ,age:"15"},
   ];
   return (
     <FlatList
@@ -19,9 +19,11 @@ const ListScreen = () => {
     
 
   }} */
+
+      
       keyExtractor={(friends) => friends.name}
       renderItem={({ item }) => {
-        return <Text style={styles.textStyle}>{item.name}</Text>;
+        return <Text style={styles.textStyle}>{item.name}-Age {item.age}</Text>
       }}
     />
   );
